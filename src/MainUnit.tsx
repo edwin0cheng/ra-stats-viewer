6f650a7d74b003ceca2fb425d23219a43b0c13c8
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-26 21:40:43 +0800

    Add error and bug fix

--- src/MainUnit.tsx
+++ src/MainUnit.tsx
@@ -7,7 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { Container, Box, FormControl, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
-import {CommitStat, Stats} from './commitStats';
+import {CommitStat, StatsOrError} from './commitStats';
 import {DateTime} from 'luxon';
 
 const useStyles = makeStyles({
@@ -54,35 +54,47 @@ function Benchmarks(props: BenchmarksProps) {
             return `${p.toFixed(2)}%`
     }
 
-    const row = function (stat: Stats) {
-        const percent_expr = function (n: number): string {
-            return percent(n, stat.expressions)
+    const row = function (stat: StatsOrError) {       
+        switch(stat.kind) {
+            case "stats": {
+                let content_rows = [];
+                const percent_expr = function (n: number): string {
+                    return percent(n, stat.expressions)
+                }    
+                
+                if (props.tab === "0") {
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c0">{stat.declarations}</TableCell>);
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c1">{stat.functions}</TableCell>);
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c2">{stat.expressions}</TableCell>);
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c3">{stat.unknown_types} ({percent_expr(stat.unknown_types)})</TableCell>);
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c4">{stat.partial_unknown_types} ({percent_expr(stat.partial_unknown_types)})</TableCell>);
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c5">{stat.type_mismatches}</TableCell>);
+                } else { 
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c6">{Math.floor(stat.database_loaded_time.milliseconds)}</TableCell>);
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c7">{Math.floor(stat.item_collection_time.milliseconds)}</TableCell>);
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c8">{Math.floor(stat.inferenece_time.milliseconds)}</TableCell>);
+                    content_rows.push(<TableCell className={classes.tablecell} align="right" key="c9">{Math.floor(stat.total_time.milliseconds)}</TableCell>);
+                }
+
+                return (<TableRow key={stat.project_name}>
+                    <TableCell className={classes.tablecell} component="th" scope="row">{shortName(stat.project_name)}</TableCell>
+                    <TableCell className={classes.tablecell} align="right">{stat.roots}</TableCell>
+                    <TableCell className={classes.tablecell} align="right">{stat.crates} [{stat.modules}]</TableCell>
+                    {content_rows}
+                </TableRow>)        
+            }
+            case "error": {
+                let nCol = 8;
+                if (props.tab === "1") {
+                    nCol = 6
+                }
+
+                return (<TableRow key={stat.project_name}>
+                    <TableCell className={classes.tablecell} component="th" scope="row">{shortName(stat.project_name)}</TableCell>
+                    <TableCell className={classes.tablecell} align="right" colSpan={nCol}>{stat.msg}</TableCell>
+                </TableRow>)
+            }
         }
-
-        let content_rows = [];
-
-        if (props.tab === "0") {
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c0">{stat.declarations}</TableCell>);
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c1">{stat.functions}</TableCell>);
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c2">{stat.expressions}</TableCell>);
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c3">{stat.unknown_types} ({percent_expr(stat.unknown_types)})</TableCell>);
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c4">{stat.partial_unknown_types} ({percent_expr(stat.partial_unknown_types)})</TableCell>);
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c5">{stat.type_mismatches}</TableCell>);
-        } else { 
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c6">{Math.floor(stat.database_loaded_time.milliseconds)}</TableCell>);
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c7">{Math.floor(stat.item_collection_time.milliseconds)}</TableCell>);
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c8">{Math.floor(stat.inferenece_time.milliseconds)}</TableCell>);
-            content_rows.push(<TableCell className={classes.tablecell} align="right" key="c9">{Math.floor(stat.total_time.milliseconds)}</TableCell>);
-        }
-
-        return (<TableRow key={stat.project_name}>
-            <TableCell className={classes.tablecell} component="th" scope="row">{shortName(stat.project_name)}</TableCell>
-            <TableCell className={classes.tablecell} align="right">{stat.roots}</TableCell>
-            <TableCell className={classes.tablecell} align="right">{stat.crates} [{stat.modules}]</TableCell>
-
-            {content_rows}
-
-        </TableRow>)
     }
 
     const rows = function () {
@@ -117,12 +129,13 @@ function Benchmarks(props: BenchmarksProps) {
         
     const time = DateTime.fromSeconds(props.commit.timestamp);
     const formattedTime = time.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
+    const refID = props.commit.commit_sha;
 
     return (
         <Container key={props.commit.commit_sha}>
             <Box display="flex" p={1}>
                 <Box flexGrow={1}>
-                    <p><a href={"https://github.com/rust-analyzer/rust-analyzer/commit/" + props.commit.commit_sha}>{"@" + props.commit.commit_sha}</a></p>
+                    <p id={refID}><a href={"https://github.com/rust-analyzer/rust-analyzer/commit/" + props.commit.commit_sha}>{"@" + props.commit.commit_sha}</a></p>
                     <Box display="flex">
                         <Box className={classes.detail}><small>Commit Time: {formattedTime}</small></Box>
                         <Box className={classes.detail}><small>Total Expression: {total_expresions}</small></Box> 
--- src/commitStats.ts
+++ src/commitStats.ts
@@ -3,65 +3,77 @@ import { Duration } from 'luxon';
 export interface CommitStat {
     timestamp: number,
     commit_sha: string,
-    stats: Stats[],
-    
+    stats: StatsOrError[],
+
     totalExpressions: number,
-    totalUnknown : number,
+    totalUnknown: number,
     totalPartial: number,
     errorRate: number,
 }
 
-type FloatWithUnit = Duration;
+export interface Error {
+    kind: "error",
+    project_name: string,
+    msg: string,
+}
 
 export interface Stats {
-    database_loaded_time: FloatWithUnit,
+    kind: "stats",
+    database_loaded_time: Duration,
     declarations: number,
     expressions: number,
     functions: number,
-    inferenece_time: FloatWithUnit,
-    item_collection_time: FloatWithUnit,
+    inferenece_time: Duration,
+    item_collection_time: Duration,
     crates: number,
     modules: number,
     partial_unknown_types: number,
     project_name: string,
     roots: number,
-    total_time: FloatWithUnit,
+    total_time: Duration,
     type_mismatches: number,
-    unknown_types: number
+    unknown_types: number,
 }
 
-function totalExpressions(stats: Stats[]) : number {
+export type StatsOrError = Stats | Error;
+
+function totalExpressions(stats: StatsOrError[]): number {
     let total_expresions = 0;
-    for(let stat of stats ) {
-        total_expresions += stat.expressions;
+    for (let stat of stats) {
+        if (stat.kind === "stats") {
+            total_expresions += stat.expressions;
+        }
     }
     return total_expresions;
 }
 
-function totalUnknown(stats: Stats[]) : number {
+function totalUnknown(stats: StatsOrError[]): number {
     let total_unknown = 0;
-    for(let stat of stats ) {
-        total_unknown += stat.unknown_types;
+    for (let stat of stats) {
+        if (stat.kind === "stats") {
+            total_unknown += stat.unknown_types;
+        }
     }
     return total_unknown;
 }
 
-function totalPartial(stats: Stats[]) : number {
+function totalPartial(stats: StatsOrError[]): number {
     let total_partial = 0;
-    for(let stat of stats ) {
-        total_partial += stat.partial_unknown_types;
+    for (let stat of stats) {
+        if (stat.kind === "stats") {
+            total_partial += stat.partial_unknown_types;
+        }
     }
     return total_partial;
 }
 
-function errorRate(stats: Stats[]) : number {
+function errorRate(stats: StatsOrError[]): number {
     let exprs = totalExpressions(stats)
     let totalError = totalUnknown(stats) + totalPartial(stats)
-    if(totalError === 0) return 0;
-    
+    if (totalError === 0) return 0;
+
     return (totalError / exprs) * 100.0
 }
-
 
 export const fetchCommits = async function (): Promise<CommitStat[]> {
     const res = await fetch('./data/commits.json')
@@ -76,21 +88,38 @@ export const fetchCommits = async function (): Promise<CommitStat[]> {
         return parseFloat(s.substring(0, s.length - 1)) * 1000
     }
 
-    const fetchStats = async function (sha: string): Promise<Stats[]> {
+    const fetchStats = async function (sha: string): Promise<StatsOrError[]> {
         const res = await fetch('./data/' + sha + '.json')
         const json = await res.json()
         return json.map((s: any) => {
-            s.database_loaded_time = Duration.fromMillis(parseTime(s.database_loaded_time))
-            s.inferenece_time = Duration.fromMillis(parseTime(s.inferenece_time))
-            s.item_collection_time = Duration.fromMillis(parseTime(s.item_collection_time))
-            s.total_time = Duration.fromMillis(parseTime(s.total_time))
+            if (s.error) {
+                s.kind = "error";
+                s.msg = s.error;
+            } else {
+                s.kind = "stats";
+                s.database_loaded_time = Duration.fromMillis(parseTime(s.database_loaded_time))
+                s.inferenece_time = Duration.fromMillis(parseTime(s.inferenece_time))
+                s.item_collection_time = Duration.fromMillis(parseTime(s.item_collection_time))
+                s.total_time = Duration.fromMillis(parseTime(s.total_time))
+            }
+
             return s
         })
     }
 
+    let promises = [];
+    let shaList: Array<[string, string]> = [];
+
     for (let [key, value] of Object.entries(json.commits)) {
-        let stats = await fetchStats(key);
-        
+        promises.push(fetchStats(key));
+        shaList.push([key, value as string]);
+    }
+
+    let allStats = await Promise.all(promises);
+    for (let i = 0; i < shaList.length; i++) {
+        const [key, value] = shaList[i];
+        const stats = allStats[i];
+
         commits.push({
             timestamp: parseInt("" + value) as number,
             commit_sha: key,
@@ -100,10 +129,18 @@ export const fetchCommits = async function (): Promise<CommitStat[]> {
             totalPartial: totalPartial(stats),
             errorRate: errorRate(stats),
         })
+
     }
 
+    if (window.location.hash) {
+        // Scroll to hash after 0.5 sec
+        setTimeout(() => {
+            console.log(window.location.hash)
+            document!.getElementById(window.location.hash.substr(1))!.scrollIntoView();
+        }, 0.5)
+    }
     // Sort by timestamp    
-    return commits.sort((a,b) => {
+    return commits.sort((a, b) => {
         return b.timestamp - a.timestamp
     })
 }
